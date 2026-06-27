bc328a89ef407858ec964a1cd02764073f35e5d5
midasbook <midasbook@users.noreply.github.com>
2026-06-27 10:44:03 +0900

    Configure toolbox lecture Apps Script endpoint

--- 2026-07-group-coaching-toolbox-lecture/config.js
+++ 2026-07-group-coaching-toolbox-lecture/config.js
@@ -1,4 +1,4 @@
 window.ToolboxLectureConfig = {
-  googleAppsScriptUrl: "",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbx5LMDFii007d2UhkQvuPjOZZiZSNqDu3t0QdvZQqqLD-QtQ4uigJ98bu6M7XkHlpwG0g/exec",
   spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1GfCOgmYPN7DLaDMyCG4rv_X_yvrNzYysNSLelLU1oPQ/edit",
 };
